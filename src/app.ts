2374f007dc1f47e9e54b5c5f097c75022f9a7057
Gabriel Cedraz <cedrazdev@gmail.com>
2024-04-12 08:19:16 -0300

    chore: install cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import fastifySwagger from '@fastify/swagger'
+import cors from '@fastify/cors'
 import { petRoutes } from './http/controllers/pets/routes'
 import { swaggerOptions } from './helpers/swagger/swagger-options'
 import fastifySwaggerUi from '@fastify/swagger-ui'
@@ -7,7 +8,14 @@ import { swaggerUiOptions } from './helpers/swagger/swagger-ui-options'
 
 export const app = fastify()
 
+// Configurações do Swagger
 app.register(fastifySwagger, swaggerOptions)
 app.register(fastifySwaggerUi, swaggerUiOptions)
 
+// Configurações de CORS
+app.register(cors, {
+  origin: '*',
+})
+
+// Configurações de rotas
 app.register(petRoutes)
